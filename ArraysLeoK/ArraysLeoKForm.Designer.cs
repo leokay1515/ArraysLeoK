ecfc7a94755017ae0357141db0968ae64f80711f
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-05-04 14:01:13 -0400

    Initial commit

---
+++ ArraysLeoK/ArraysLeoKForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ArraysLeoK
+{
+    partial class frmArraysLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(91, 35);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(89, 35);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(58, 87);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(151, 147);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(79, 250);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(118, 47);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate Average";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmArraysLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 309);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmArraysLeoK";
+            this.Text = "Arrays by Leo Kay";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ ArraysLeoK/ArraysLeoKForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArraysLeoK
+{
+    public partial class frmArraysLeoK : Form
+    {
+        //global variables
+        const int MAX_ARRAY_SIZE = 10;
+        int[] arrayOfNumbers = new int[MAX_ARRAY_SIZE];
+
+        public frmArraysLeoK()
+        {
+            InitializeComponent();
+
+            //disable the calculate button
+            this.btnCalculate.Enabled = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //local variables
+            int randomNumbers;
+            int counter;
+            Random randomNumberGenerator = new Random();
+
+            //clear the listbox
+            this.lstNumbers.Items.Clear();
+
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                //Generate a random number between 1 and the array size
+                randomNumbers = randomNumberGenerator.Next(1, MAX_ARRAY_SIZE);
+
+                //assign randomNumber to the cell at the counter posistion in the array
+                arrayOfNumbers[counter] = randomNumbers;
+
+                //add the number to the listbox
+                this.lstNumbers.Items.Add(randomNumbers);
+
+                //refresh the form
+                this.Refresh();
+            }
+
+            //enable the calculate button
+            this.btnCalculate.Enabled = true;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //local variables
+            double average = 0;
+            double sum = 0;
+            int counter;
+            
+            //calculate the sum of all the numbers in the array
+            for (counter = 0; counter < arrayOfNumbers.Length; counter++)
+            {
+                sum = sum + arrayOfNumbers[counter];
+            }
+
+            //calculate the average
+            average = sum / arrayOfNumbers.Length;
+
+            //display the average
+            MessageBox.Show("The average is " + average, "Average of Array");
+
+            //disable the calculate button
+            this.btnCalculate.Enabled = false;
+        }
+    }
+}
